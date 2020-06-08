ca58db67743f2af76c02399b1015ab8f3c5e7ef3
sarahtan931 <sarah.tan931@gmail.com>
2020-06-08 11:16:46 -0400

    Update YoutubeToSpotify.py

--- YoutubeToSpotify.py
+++ YoutubeToSpotify.py
@@ -10,7 +10,7 @@ import youtube_dl
 from secrets import spotify_user_id, spotify_token
 
 
-class CreatePlaylist:
+class YoutubeToSpotify:
 
     def __init__(self):
         self.user_id = spotify_user_id
